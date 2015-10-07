9403520c3dca5ea5511ccf3b7ab728edb14ba9c4
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-10-07 11:14:24 +0200

    Turn index.js into simple packed cover example

---
+++ index.js
@@ -0,0 +1,11 @@
+var magic = require('./lib/');
+var covers = require('./lib/covers');
+var path = magic.render.path;
+var randInt = magic.random.int;
+var choice = magic.random.choice;
+var circle = magic.points.circle;
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d', {alpha: false});
+
+covers.packed(ctx);
